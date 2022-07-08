a339b4a9279073b2f3a18440c5ab02eb8e283e03
Jim Wolf <76077313+jim-wolf-i-am@users.noreply.github.com>
2022-07-08 06:11:16 -0500

    deleted some files

--- forms_php/addition.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-<!--Your code goes here-->
-  <?= "The sum of ${_GET['add1']} and ${_GET['add2']} is: "?>
-  <b><?= $_GET['add1'] + $_GET['add2'];?></b>
-  </br>
-<a href="index.php">Reset</a>
-</body>
-</html>
--- forms_php/division.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-<!--Your code goes here-->
-  <?= "The quotient of ${_GET['divide1']} divided by ${_GET['divide2']} is: "?>
-  <b><?= $_GET['divide1'] / $_GET['divide2'];?></b>
-  </br>
-<a href="index.php">Reset</a>
-</body>
-</html>
