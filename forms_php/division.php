270b5203898804bcd59f1763fd781dac4e72c3ba
Jim Wolf <76077313+jim-wolf-i-am@users.noreply.github.com>
2022-07-07 22:22:31 -0500

    just exploring php

---
+++ forms_php/division.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<!--Your code goes here-->
+  <?= "The quotient of ${_GET['divide1']} divided by ${_GET['divide2']} is: "?>
+  <b><?= $_GET['divide1'] / $_GET['divide2'];?></b>
+  </br>
+<a href="index.php">Reset</a>
+</body>
+</html>
---
+++ forms_php/index.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+<!--Your code goes here-->
+<h2>Addition Calculator</h2>
+<form method="get" action="addition.php">
+  <input type="number" name="add1">
+  <input type="number" name="add2">
+  <button type="submit">Add</button>
+</form>
+<h2>Division Calculator</h2>
+<form method="get" action="division.php">
+  <input type="number" name="divide1">
+  <input type="number" name="divide2">
+  <button type="submit">Divide</button>
+</form>
+<a href="index.php">Reset</a>
+</body>
+</html>
